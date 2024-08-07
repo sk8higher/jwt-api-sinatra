eb4ba03493bd2f7e90faadc46fbfdb1cef16f3a6
Nelly Simkova <sk8high3r@ya.ru>
2024-08-07 20:14:12 +0300

    Add sccess token generation to Api

--- app.rb
+++ app.rb
@@ -5,3 +5,4 @@ require_relative './models/user'
 require_relative './controllers/api'
 
 Mongoid.load!(File.join(File.dirname(__FILE__), 'config', 'mongoid.yml'))
+Mongoid.raise_not_found_error = false
--- controllers/api.rb
+++ controllers/api.rb
@@ -4,12 +4,30 @@ require 'sinatra/base'
 
 class Api < Sinatra::Base
   get '/tokens' do
-    user = User.where(:id => params[:user_id])
+    user = User.find(params[:user_id])
 
-    if user.exists?
-      user.to_json
+    unless user.nil?
+      content_type :json
+      { access_token: access_token(user[:id]) }.to_json
     else
       halt 404
     end
+  end
+
+  private
+
+  def access_token(user_id)
+    JWT.encode(payload(user_id), ENV['JWT_SECRET'], 'HS512')
+  end
+
+  def payload(user_id)
+    {
+      exp: Time.now.to_i + 60 * 60,
+      iat: Time.now.to_i,
+      iss: ENV['JWT_ISSUER'],
+      user: {
+        user_id: user_id
+      }
+    }
   end
 end
