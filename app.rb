6a06167e12d6f650c7ccea7a380f8b44db03689d
Nelly Simkova <sk8high3r@ya.ru>
2024-08-07 18:57:36 +0300

    Add config and user model

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'mongoid'
+
+require_relative './models/user'
+
+Mongoid.load!(File.join(File.dirname(__FILE__), 'config', 'mongoid.yml'))
 
 get '/' do
-  'Testing'
+  user = User.create!
+  user.to_json
 end
