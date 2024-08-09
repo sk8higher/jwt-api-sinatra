bcf91af15c30a953665a809d0536919e02afe9ba
Nelly Simkova <sk8high3r@ya.ru>
2024-08-09 20:09:25 +0300

    Add ip into the payload

--- controllers/api.rb
+++ controllers/api.rb
@@ -10,8 +10,8 @@ class Api < Sinatra::Base
     unless @user.nil?
       content_type :json
       {
-        access_token: access_token(@user[:id]),
-        refresh_token: refresh_token(@user[:id])
+        access_token: access_token(@user[:id], request.ip),
+        refresh_token: refresh_token(@user[:id], request.ip)
       }.to_json
     else
       halt 404
@@ -20,25 +20,26 @@ class Api < Sinatra::Base
 
   private
 
-  def access_token(user_id)
-    JWT.encode(payload(user_id, 'access'), ENV['JWT_SECRET'], 'HS512')
+  def access_token(user_id, ip)
+    JWT.encode(payload(user_id, 'access', ip), ENV['JWT_SECRET'], 'HS512')
   end
 
-  def refresh_token(user_id)
+  def refresh_token(user_id, ip)
     if @user.refresh_token.empty?
-      @user.refresh_token = BCrypt::Password.create(JWT.encode(payload(user_id, 'refresh'), ENV['JWT_SECRET'], 'HS512'))
+      @user.refresh_token = BCrypt::Password.create(JWT.encode(payload(user_id, 'refresh', ip), ENV['JWT_SECRET'], 'HS512'))
       @user.save
     end
     @user.refresh_token
   end
 
-  def payload(user_id, type)
+  def payload(user_id, type, ip)
     {
       exp: type == 'access' ? Time.now.to_i + 60 * 60 : Time.now.to_i + 60 * 360,
       iat: Time.now.to_i,
       iss: ENV['JWT_ISSUER'],
       user: {
-        user_id: user_id
+        user_id: user_id,
+        ip: ip
       }
     }
   end
